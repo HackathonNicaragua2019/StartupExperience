721a0f71c6c8a79951338f3e71a1abb1cc61720f
Luis Reyes <rodolforeyes2016@outlook.es>
2020-06-14 22:05:20 -0600

    actualizando

---
+++ controladores/empresa.controlador.php
@@ -0,0 +1,68 @@
+<?php
+
+class ControladorEmpresa{
+
+    /*=============================================
+	CREAR CATEGORIAS
+    =============================================*/
+    
+    static public function ctrCrearEmpresa(){
+
+		if(isset($_POST["nuevaCategoria"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevaCategoria"])){
+
+				$tabla = "categorias";
+
+				$datos = $_POST["nuevaCategoria"];
+
+				$respuesta = ModeloCategorias::mdlIngresarCategoria($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "La categoría ha sido guardada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "categorias";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡La categoría no puede ir vacía o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "categorias";
+
+							}
+						})
+
+			  	</script>';
+
+			}
+
+		}
+
+    }
+
+}
